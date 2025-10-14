c0b57fb5683c7349f0407dda753bb0e83e6828c8
ruthcastillo22 <castillohuamaniruthm@gmail.com>
2025-10-14 10:32:40 -0500

    agrego la aplicacion basica de prueba con toda la informacion

---
+++ README.md
@@ -0,0 +1,2 @@
+# Aplicacion de gestion de tareas 
+esta aplicacion me permite practicar los conceptos basicos que aprendi sobre html, css, js
---
+++ script.js
@@ -0,0 +1,40 @@
+function mostrarMensaje(){
+    console.log("hola como estas")
+}
+
+function capturaTexto(){
+    // 1. capturamos el elemento input
+    let input=document.querySelector("#nuevaTarea")
+
+    // 10. para comprobar si elinput esta vacio
+    if (input.value=="")
+    {
+        alert("el input debe contener un texto")
+        return
+    }
+
+    // 2. capturamos el valor de input
+    let valorInput=input.value
+
+    // 3. capturamos el elemento ul
+    let ul=document.querySelector("#listaTareas")
+    // 4. creamos un elemento li
+    let li=document.createElement("li")
+    // 5. le asignamos como contenido del li el valor capturado de input
+    li.textContent=valorInput
+
+    // 7. creamos un boton eliminar
+    let button=document.createElement("button")
+    button.textContent="Eliminar"
+
+    // 8. para que permita eliminar la tarea que esta al costado del boton eliminar
+    button.onclick=function(){li.remove()}
+
+    // 7.
+    li.appendChild(button)
+    // 6. le agregamos como hijo el "li" al elemento padre "ul"
+    ul.appendChild(li)
+    
+    // 9. para que elimine lo que esta en la barra de escritura cada que agregue algo
+    document.querySelector("#nuevaTarea").value=""
+}
