74abf713a78dc95eaacb3ccfe5b11e6c406f15d5
ruthcastillo22 <castillohuamaniruthm@gmail.com>
2025-10-16 08:38:21 -0500

    feat:crear app tareas
    se creo la funcionque permite capturar el texto agregar una lista y podereliminarlo

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function mostrarMensaje(){
     console.log("hola como estas")
 }
 
-function capturaTexto(){
+function agregarTarea(){
     // 1. capturamos el elemento input
     let input=document.querySelector("#nuevaTarea")
 
@@ -35,6 +35,8 @@ function capturaTexto(){
     // 6. le agregamos como hijo el "li" al elemento padre "ul"
     ul.appendChild(li)
     
-    // 9. para que elimine lo que esta en la barra de escritura cada que agregue algo
+    // 9. para que elimine lo que esta en la barra de escritura cada que agregue algo. Limpia lo que tiene previamente escrito
     document.querySelector("#nuevaTarea").value=""
+    // 11. funcion focus en el input: lo mantienen activo. Listo para ingresar cualquier otro texto. 
+    input.focus()
 }
